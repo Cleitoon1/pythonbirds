2b49fcb946a24185114c9f31afb9eaceab42d58a
Cleitoon1 <cleiton.alvesjc@gmail.com>
2015-09-22 09:43:43 -0300

    Fase Finalizada
    Fase Finalizada

--- fase.py
+++ fase.py
@@ -48,6 +48,7 @@ class Fase():
         pass
 
     def adicionar_porco(self, *porcos):
+        self ._porcos.extend(porcos)
         """
         Adiciona porcos em uma fase
 
@@ -56,6 +57,7 @@ class Fase():
         pass
 
     def adicionar_passaro(self, *passaros):
+        self._passaros.extend(passaros)
         """
         Adiciona pássaros em uma fase
 
@@ -75,7 +77,15 @@ class Fase():
 
         :return:
         """
+        porco, passaro = False, False
+        porco = [True for i in self._porcos if i.status == ATIVO]
+        passaro = [True for i in self._passaros if i.status == ATIVO]
+        if not porco:
+            return VITORIA
+        elif not passaro:
+            return DERROTA
         return EM_ANDAMENTO
+
 
     def lancar(self, angulo, tempo):
         """
@@ -88,8 +98,11 @@ class Fase():
         :param angulo: ângulo de lançamento
         :param tempo: Tempo de lançamento
         """
-        pass
-
+        # [i.lancar(angulo,tempo) for i in self._passaros if i.status == ATIVO and not i.foi_lancado()]
+        for i in self._passaros:
+            if i.status == ATIVO and not i.foi_lancado():
+                i.lancar(angulo, tempo)
+                break
 
     def calcular_pontos(self, tempo):
         """
@@ -100,10 +113,21 @@ class Fase():
         :param tempo: tempo para o qual devem ser calculados os pontos
         :return: objeto do tipo Ponto
         """
-        pontos=[]
-
+        boo = True
+        pontos = []
+        # 1.0 Calcular a posição de cada passaro.
+        # 1.1 Mandar Calcular a posição passando o tempo.
+        for passaro in self._passaros:
+            passaro.calcular_posicao(tempo)
+        # 2.0 Verificar se colidiu com porcos ou obstaculo
+            for ator in self._porcos + self._obstaculos:
+                passaro.colidir(ator, self.intervalo_de_colisao)
+                passaro.colidir_com_chao()
+        # 3.0 Transformar tudo em pontos (atores, obstaculos e passaros)
+        # 3.1 Passar tudo para a lista e retorna-la
+        for ator in self._porcos + self._obstaculos + self._passaros:
+            pontos.append(self._transformar_em_ponto(ator))
         return pontos
 
-    def _transformar_em_ponto(self, ator):
-        return Ponto(ator.x, ator.y, ator.caracter())
-
+    def _transformar_em_ponto(self, c):
+        return Ponto(c.x, c.y, c.caracter())
